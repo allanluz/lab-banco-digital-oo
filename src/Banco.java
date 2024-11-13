c0a63ff2ef1ee919ba4574e6f50bdc2345e978e2
allanluz <allan.luz@live.com>
2024-11-12 21:14:51 -0300

    Novo método na classe Banco e implementação do Lombok

--- src/Banco.java
+++ src/Banco.java
@@ -1,24 +1,24 @@
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class Banco {
 
-	private String nome;
-	private List<Conta> contas;
+    private String nome;
+    private List<Conta> contas;
 
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public List<Conta> getContas() {
-		return contas;
-	}
-
-	public void setContas(List<Conta> contas) {
-		this.contas = contas;
-	}
-
+    // Método para listar todos os clientes
+    public void listarClientes() {
+        if (contas == null || contas.isEmpty()) {
+            System.out.println("Nenhuma conta encontrada.");
+            return;
+        }
+        
+        System.out.println("Lista de clientes:");
+        for (Conta conta : contas) {
+            System.out.println(conta.cliente.getNome());
+        }
+    }
 }
